e53e54d0467d1c2b3873d22281bf5dea567d8493
Christian Ryan Macarse <crrmacarse@gmail.com>
2019-02-19 14:10:52 +0800

    Payload test fix

--- classes/rest.php
+++ classes/rest.php
@@ -215,8 +215,10 @@
                 $token = $this->getBearerToken();
                 $payload = JWT::decode($token, JWT_SECRET_KEY, array('HS256'));
             
+                die($payload{"userId"});
+                
                 // PDO query to check for user
-                $sql = "SELECT idAccountUser FROM AccountUser WHERE idAccountUser = '$payload[userId]'";
+                $sql = "SELECT idAccountUser FROM AccountUser WHERE idAccountUser = '$payload{userId}'";
                 $result = odbc_exec($this->connection, $sql);   
                 $user = odbc_fetch_array($result);
 
